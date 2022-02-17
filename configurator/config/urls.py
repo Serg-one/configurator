69273eaabe1e055cfb03b91b34ba7126195e12b8
Serg-one <bocharov.serg@gmail.com>
2022-02-17 23:26:01 +0200

    fix urls

--- configurator/config/urls.py
+++ configurator/config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from config import settings
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +10,4 @@ urlpatterns = [
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
